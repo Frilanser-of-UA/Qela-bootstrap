68b49c48c780207cc34829e6344c3a1f4b07cf3f
Andrey Ivanov <foresteraws@gmail.com>
2023-01-26 15:31:59 +0200

    26.01.2023 page reg

---
+++ #src/js/files/menu.js
@@ -0,0 +1,26 @@
+
+window.onload = function () {
+	document.addEventListener("click", documentActions);
+	// Actions ( делегирование собитий click)
+	function documentActions(e) {
+		const targetElement = e.target;
+		// ====== Меню
+		if (targetElement.classList.contains('qela-btn-menu') || targetElement.classList.contains('qela-burger')) {
+			if (document.querySelector('.qela-btn-menu') || document.querySelector('.qela-burger')) {
+				document.querySelector('.qela-menu').classList.toggle('active');
+				document.querySelector('.qela-btn-menu').classList.toggle('active');
+			}
+		} else if (!targetElement.closest('#gela-nav') && document.querySelector('.qela-menu.active')) {
+			document.querySelector('.qela-menu').classList.remove('active');
+			document.querySelector('.qela-btn-menu').classList.remove('active');
+		}
+		//== випадашки search
+		if (targetElement.classList.contains('qela-search-btn')) {
+			if (document.querySelector('.qela-search-btn')) {
+				document.querySelector('.qela-wrapper').classList.toggle('qela--visible');
+			}
+		} else if (!targetElement.closest('.qela-wrapper') && document.querySelector('.qela-wrapper.qela--visible')) {
+			document.querySelector('.qela-wrapper').classList.remove('qela--visible');
+		}
+	}
+}
