0475947bd13b6145688292e2a03fdcba38d61333
Andrey Ivanov <foresteraws@gmail.com>
2023-06-22 12:28:25 +0300

    Дата 22.06.23 Добавлено страница organization.html

--- Qela/js/app.js
+++ Qela/js/app.js
@@ -1,4 +1,4 @@
-
+// menu
 window.onload = function () {
 	document.addEventListener("click", documentActions);
 	// Actions ( делегирование собитий click)
@@ -24,70 +24,28 @@ window.onload = function () {
 		}
 	}
 }
-// // Полифилл для метода forEach для NodeList
-if (window.NodeList && !NodeList.prototype.forEach) {
-	NodeList.prototype.forEach = function (callback, thisArg) {
-		thisArg = thisArg || window;
-		for (var i = 0; i < this.length; i++) {
-			callback.call(thisArg, this[i], i, this);
-		}
-	};
-}
 
-document.querySelectorAll('.qela-dropdown').forEach(function (dropDownWrapper) {
-	const dropDownBtn = dropDownWrapper.querySelector('.dropdown-btn');
-	const dropDownList = dropDownWrapper.querySelector('.dropdown-list');
-	const dropDownListItems = dropDownList.querySelectorAll('.dropdown-item');
-
-	// Клик по кнопке. Открыть/Закрыть select
-	dropDownBtn.addEventListener('click', function (e) {
-		dropDownList.classList.toggle('visible');
-		this.classList.toggle('active');
-	});
-
-	// Выбор элемента списка. Запомнить выбранное значение. Закрыть дропдаун
-	dropDownListItems.forEach(function (listItem) {
-		listItem.addEventListener('click', function (e) {
-			e.stopPropagation();
-			dropDownBtn.innerText = this.innerText;
-			if (this.classList.contains('not-visible')) {
-				this.classList.remove('not-visible');
-			} else {
-				const activeLink = dropDownList.querySelector('.dropdown-item.not-visible');
-				if (activeLink) {
-					activeLink.classList.remove('not-visible');
-				}
-				this.classList.add('not-visible');
-			}
-			dropDownBtn.focus();
-			dropDownList.classList.remove('visible');
-			dropDownBtn.classList.remove('active');
-			if (e.target.classList.contains('label')) {
-				dropDownBtn.classList.remove('selected');
-			} else {
-				dropDownBtn.classList.add('selected');
-
-			}
-		});
-	});
-
-	// Клик снаружи дропдауна. Закрыть дропдаун
-	document.addEventListener('click', function (e) {
-		if (e.target !== dropDownBtn) {
-			dropDownBtn.classList.remove('active');
-			dropDownList.classList.remove('visible');
-		}
-	});
-
-	// Нажатие на Tab или Escape. Закрыть дропдаун
-	document.addEventListener('keydown', function (e) {
-		if (e.key === 'Tab' || e.key === 'Escape') {
-			dropDownBtn.classList.remove('active');
-			dropDownList.classList.remove('visible');
-		}
-	});
-});
-// ============================================
-
+// tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// read more text
+let coll = document.querySelectorAll('[data-ql-target]')
+for (let i = 0; i < coll.length; i++) {
+	coll[i].addEventListener('click', function () {
+		let self = event.currentTarget;
+		let valueAtribute = self.getAttribute('data-ql-target');
+		let content = document.querySelector(`.qela-description[id="${valueAtribute}"]`);
+		content.classList.toggle('active');
+		this.classList.toggle('active');
+		if (content.style.height) {
+			content.style.height = null
+			this.textContent = 'Read more';
+		} else {
+			content.style.height = content.scrollHeight + 'px';
+			this.textContent = 'Cloze';
+		}
+	})
+}
+
+// read more text
