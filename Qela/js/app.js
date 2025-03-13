33de4e6cf32e6e4200dc7836a08844b2fe9588e6
“Andrey <foresteraws@gmail.com>
2025-03-13 10:29:03 +0200

    Eddit stile and js

--- Qela/js/app.js
+++ Qela/js/app.js
@@ -1,17 +1,26 @@
 const btnMenu = document.querySelector('.qela-btn-menu');
 const menuAside = document.querySelector('.qela-menu');
+const sidebarTooltips = document.querySelectorAll('[data-bs-custom-class="qela-sidebar-tooltip"],[data-bs-custom-class="qela-sidebar-tooltip settings"]');
+// open/close menu
 if (menuAside) {
-    btnMenu.addEventListener('click', () => {
-        btnMenu.classList.toggle('close');
-        menuAside.classList.toggle('close');
-        // document.body.classList.toggle('active');
-    });
-    const observer = new ResizeObserver(entrirs => {
-        const elem = entrirs[0];
-        let widthAside = elem.borderBoxSize[0].inlineSize;
-        document.body.style.paddingLeft = widthAside + 'px';
-    })
-    observer.observe(menuAside);
+	btnMenu.addEventListener('click', () => {
+		btnMenu.classList.toggle('close');
+		menuAside.classList.toggle('close');
+		// document.body.classList.toggle('active');
+	});
+	const observer = new ResizeObserver(entrirs => {
+		const elem = entrirs[0];
+		let widthAside = elem.borderBoxSize[0].inlineSize;
+		document.body.style.paddingLeft = widthAside + 'px';
+	})
+	observer.observe(menuAside);
+}
+// hide tooltips when menu open
+for (let i = 0; i < sidebarTooltips.length; i++) {
+	sidebarTooltips[i].addEventListener('show.bs.tooltip', event => {
+		if (!menuAside.classList.contains('close') && window.innerWidth > 1440) return event.preventDefault();
+		else if (menuAside.classList.contains('close') && window.innerWidth < 1440) return event.preventDefault();
+	})
 }
 // tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
@@ -22,46 +31,8 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
 if (toastTrigger) {
-    toastTrigger.addEventListener('click', () => {
-        const toast = new bootstrap.Toast(toastLiveExample)
-
-        toast.show()
-    })
+	toastTrigger.addEventListener('click', () => {
+		const toast = new bootstrap.Toast(toastLiveExample)
+		toast.show()
+	})
 }
-// input search
-const inputSearchWrap = document.querySelectorAll('.qela-search');
-if (inputSearchWrap) {
-    for (let i = 0; i < inputSearchWrap.length; i++) {
-        const inputSearch = inputSearchWrap[i].querySelector('.form-control');
-        const inputButton = inputSearchWrap[i].querySelector('.ic-cloze');
-        console.log(inputButton);
-        inputSearch.addEventListener('input', function () {
-            if (inputSearch.value != '') inputSearchWrap[i].classList.add('active');
-            else inputSearchWrap[i].classList.remove('active');
-        })
-        inputButton.addEventListener('click', function () {
-            inputSearch.value = '';
-            inputSearchWrap[i].classList.remove('active');
-        })
-    }
-}
-// input search
-// read more text
-let coll = document.querySelectorAll('[data-qela-target]')
-for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener('click', function () {
-        let self = event.currentTarget;
-        let valueAtribute = self.getAttribute('data-qela-target');
-        let content = document.querySelector(`.qela-description[id="${valueAtribute}"]`);
-        content.classList.toggle('active');
-        this.classList.toggle('active');
-        if (content.style.height) {
-            content.style.height = null
-            this.textContent = 'Read more';
-        } else {
-            content.style.height = content.scrollHeight + 'px';
-            this.textContent = 'Cloze';
-        }
-    })
-}
-// read more text
