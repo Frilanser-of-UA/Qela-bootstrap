6c9f2d48712909e6865763a60e25e91bfd97f4c0
“Andrey <foresteraws@gmail.com>
2025-03-19 18:42:10 +0200

    Add chat components + chat page

--- Qela/js/app.js
+++ Qela/js/app.js
@@ -3,24 +3,24 @@ const menuAside = document.querySelector('.qela-menu');
 const sidebarTooltips = document.querySelectorAll('[data-bs-custom-class="qela-sidebar-tooltip"],[data-bs-custom-class="qela-sidebar-tooltip settings"]');
 // open/close menu
 if (menuAside) {
-	btnMenu.addEventListener('click', () => {
-		btnMenu.classList.toggle('close');
-		menuAside.classList.toggle('close');
-		// document.body.classList.toggle('active');
-	});
-	const observer = new ResizeObserver(entrirs => {
-		const elem = entrirs[0];
-		let widthAside = elem.borderBoxSize[0].inlineSize;
-		document.body.style.paddingLeft = widthAside + 'px';
-	})
-	observer.observe(menuAside);
+    btnMenu.addEventListener('click', () => {
+        btnMenu.classList.toggle('close');
+        menuAside.classList.toggle('close');
+        // document.body.classList.toggle('active');
+    });
+    const observer = new ResizeObserver(entrirs => {
+        const elem = entrirs[0];
+        let widthAside = elem.borderBoxSize[0].inlineSize;
+        document.body.style.paddingLeft = widthAside + 'px';
+    })
+    observer.observe(menuAside);
 }
 // hide tooltips when menu open
 for (let i = 0; i < sidebarTooltips.length; i++) {
-	sidebarTooltips[i].addEventListener('show.bs.tooltip', event => {
-		if (!menuAside.classList.contains('close') && window.innerWidth > 1440) return event.preventDefault();
-		else if (menuAside.classList.contains('close') && window.innerWidth < 1440) return event.preventDefault();
-	})
+    sidebarTooltips[i].addEventListener('show.bs.tooltip', event => {
+        if (!menuAside.classList.contains('close') && window.innerWidth > 1440) return event.preventDefault();
+        else if (menuAside.classList.contains('close') && window.innerWidth < 1440) return event.preventDefault();
+    })
 }
 // tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
@@ -31,8 +31,8 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
 if (toastTrigger) {
-	toastTrigger.addEventListener('click', () => {
-		const toast = new bootstrap.Toast(toastLiveExample)
-		toast.show()
-	})
+    toastTrigger.addEventListener('click', () => {
+        const toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
 }
