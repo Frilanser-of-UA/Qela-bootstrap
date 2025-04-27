336a8fd95a399f7a86944b06ab59b4319392e513
“Andrey <foresteraws@gmail.com>
2025-04-27 11:06:29 +0300

    Fix tooltip

--- #src/js/app.js
+++ #src/js/app.js
@@ -23,8 +23,8 @@ for (let i = 0; i < sidebarTooltips.length; i++) {
     })
 }
 // tooltip
-// const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 // toast
 // const toastElList = document.querySelectorAll('.toast');
 // const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl));
